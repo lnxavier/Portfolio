c1c15c90b82a793faf37efeba77dcf9988c3c034
Lucas <lxavier.dev@hotmail.com>
2025-04-09 19:25:32 -0300

    teste para solucionar um erro com as imagens

--- src/database/tecnologiesUsing.js
+++ src/database/tecnologiesUsing.js
@@ -1,10 +1,11 @@
 export const usingTecnologies = [
-    { id: 1, name: 'HTML5', image: '../../public/assets/images/logos/html5-logo.png' },
-    { id: 2, name: 'CSS3', image: '../../public/assets/images/logos/css3-logo.png' },
-    { id: 3, name: 'JAVA SCRIPT', image: '../../public/assets/images/logos/javascript-logo.png' },
-    { id: 4, name: 'REACT', image: '../../public/assets/images/logos/react-logo.png' },
-    { id: 5, name: 'NODEJS', image: '../../public/assets/images/logos/node-logo.png' },
-    { id: 6, name: 'SASS', image: '../../public/assets/images/logos/sass-logo.png' },
-    { id: 7, name: 'GIT', image: '../../public/assets/images/logos/git-logo.png' },
-    { id: 8, name: 'FIGMA', image: '../../public/assets/images/logos/figma-logo.png' }
-];
+    { id: 1, name: 'HTML5', image: '/assets/images/logos/html5-logo.png' },
+    { id: 2, name: 'CSS3', image: '/assets/images/logos/css3-logo.png' },
+    { id: 3, name: 'JAVA SCRIPT', image: '/assets/images/logos/javascript-logo.png' },
+    { id: 4, name: 'REACT', image: '/assets/images/logos/react-logo.png' },
+    { id: 5, name: 'NODEJS', image: '/assets/images/logos/node-logo.png' },
+    { id: 6, name: 'SASS', image: '/assets/images/logos/sass-logo.png' },
+    { id: 7, name: 'GIT', image: '/assets/images/logos/git-logo.png' },
+    { id: 8, name: 'FIGMA', image: '/assets/images/logos/figma-logo.png' }
+  ];
+  

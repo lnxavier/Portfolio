bf7c0b4e3be517ef94b1724fc79b63767d3c88da
Lucas <lxavier.dev@hotmail.com>
2025-05-10 18:23:26 -0300

    project added

--- src/database/projects.js
+++ src/database/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     id: 1,
     name: 'LX Shoes (Ecommerce)',
     status: 'Desenvolvido',
-    image: '../assets/images/projects/01.png',
+    image: '../assets/images/projects/lxshoes.png',
     link: 'https://lxshoes.onrender.com/',
     github: 'https://github.com/lnxavier/Ecommerce',
     description: `Este projeto foi criado como parte do meu processo de aprendizado. A loja LX Shoes é fictícia, mas simula uma aplicação real de e-commerce, com foco em estruturação de layout, organização de código, responsividade e boas práticas com Sass.`,
@@ -13,24 +13,27 @@ export const projects = [
 
   {
     id: 2,
+    name: 'LX Motors (Concessionária)',
+    status: 'Em desenvolvimento',
+    image: '../assets/images/projects/lxmotors.png',
+    github: 'https://github.com/lnxavier/Concessionaria',
+    link: 'https://lx-motors.onrender.com/',
+    description: `Este projeto está em desenvolvimento e tem como objetivo criar um site para uma <strong>loja virtual de motos</strong>.<br>
+A proposta é oferecer uma plataforma intuitiva e responsiva onde os clientes possam visualizar a lista de motos disponíveis para compra, acessar os detalhes de cada modelo e favoritar suas motos preferidas, criando assim uma lista personalizada.<br><br>
+Além disso, o site contará com uma área de gerenciamento voltada para o <strong>administrador (dono da loja)</strong>, permitindo o cadastro, edição e exclusão de motos, bem como o controle das informações exibidas aos clientes.<br><br>
+O projeto busca aplicar conceitos modernos de desenvolvimento <strong>front-end</strong> e <strong>back-end</strong>, unindo funcionalidade e boa experiência do usuário.`,
+    technologies: ['HTML', 'CSS', 'JavaScript', 'SASS', 'REACT', 'VITE']
+  },
+
+  {
+    id: 3,
     name: 'Spotify',
     status: 'Desenvolvido',
-    image: '../assets/images/projects/02.png',
+    image: '../assets/images/projects/spotify.png',
     github: 'https://github.com/lnxavier/projetoSpotify',
     link: 'https://projetospotify-i2bu.onrender.com/',
     description: `Este projeto consiste em uma réplica do Spotify, desenvolvida com o objetivo de praticar e consolidar conhecimentos em desenvolvimento web.<br>
     A ideia foi reproduzir a interface e funcionalidades básicas da plataforma de streaming de música, aplicando conceitos modernos de front-end e back-end.`,
-    technologies: ['HTML', 'CSS', 'JavaScript', 'REACT', 'VITE']
-  },
-  {
-    id: 3,
-    name: 'Concessionária',
-    status: 'Em desenvolvimento',
-    image: '../assets/images/projects/03.jpg',
-    github: '',
-    link: '',
-    description: `Este projeto consiste em uma simulação de uma <strong>concessionária online</strong>.<br>
-    A proposta é criar uma interface intuitiva e responsiva que permite a visualização de veículos disponíveis, com funcionalidades básicas de cadastro, listagem e detalhes de automóveis, aplicando conceitos modernos de front-end e back-end.<br><br>`,
     technologies: ['HTML', 'CSS', 'JavaScript', 'REACT', 'VITE']
   }
 ];
